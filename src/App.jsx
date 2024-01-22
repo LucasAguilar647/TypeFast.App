3ec6170ef86eb5c8e6d20aae00d5de356ce223e0
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-01-22 13:41:35 -0300

    separe en un componente la pantalla de juego y su css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./index.css";
 import { Word } from "./components/word";
 import { PantallaPerdiste } from "./components/PantallaPerdiste";
+import { PantallaJuego } from "./components/PantallaJuego";
 
 const App = () => {
   const [points, setPoints] = useState(0);
@@ -32,28 +33,16 @@ const App = () => {
   return (
     <div className="recuadro">
       {vidas > 0 ? (
-        <>
-          <h1>TypeFast</h1>
-          <h3>Vidas: {vidas} </h3>
-          <hr />
-          <div className="recuadro-juego">
-            <Word onWordChange={handleWordChange} points={points} />
-          </div>
-          <hr />
-          <h3>Points: {points} </h3>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="input-juego"
-              placeholder="Type Here"
-              autoComplete="off"
-              value={inputValue}
-              onChange={handleChange}
-            />
-          </form>
-        </>
+        <PantallaJuego
+          vidas={vidas}
+          handleWordChange={handleWordChange}
+          points={points}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          inputValue={inputValue}
+        />
       ) : (
-       <PantallaPerdiste points={points}/>
+        <PantallaPerdiste points={points} />
       )}
     </div>
   );
---
+++ src/components/PantallaJuego.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Word } from "./word";
+import './juego.css'
+
+export const PantallaJuego = ({
+  vidas,
+  handleWordChange,
+  points,
+  handleSubmit,
+  handleChange,
+  inputValue,
+}) => {
+  return (
+    <>
+      <h1>TypeFast</h1>
+      <h3>Vidas: {vidas} </h3>
+      <hr />
+      <div className="recuadro-juego">
+        <Word onWordChange={handleWordChange} points={points} />
+      </div>
+      <hr />
+      <h3>Points: {points} </h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input-juego"
+          placeholder="Type Here"
+          autoComplete="off"
+          value={inputValue}
+          onChange={handleChange}
+        />
+      </form>
+    </>
+  );
+};
