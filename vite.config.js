17bb5c5c937174e1635731c2d482c5ba9c017715
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-12-05 15:20:03 -0300

    solucion

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
+
 server: {
     host: '0.0.0.0',  // Permite conexiones desde cualquier IP
     port: 5173,        // El puerto en el que se ejecuta tu proyecto
-=======
+  },
   build: {
     outDir: 'docs', // Cambia el directorio de salida a 'docs'
->>>>>>> cb26833f43238fb14feafe56e3843d7954e7d78f
   },
 })
