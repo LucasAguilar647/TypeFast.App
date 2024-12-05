d034bef6810aa2b5f157e0f1f2536ea01bcda49f
raspyLucas <lucasaguilar647@gmail.com>
2024-12-05 18:09:03 +0000

    agregue el deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configuración inicial
-set -e # Detener el script si ocurre un error
+set -e
 
 echo "Actualizando el proyecto desde GitHub..."
 git pull origin main
@@ -10,19 +10,24 @@ echo "Instalando dependencias..."
 npm install
 
 echo "Eliminando la carpeta de compilación anterior..."
-rm -rf docs # Elimina la carpeta docs existente
+rm -rf docs
 
 echo "Construyendo el proyecto..."
 npm run build
 
 echo "Renombrando la carpeta de compilación a 'docs'..."
-mv dist docs # Renombra 'dist' a 'docs' para GitHub Pages
+mv dist docs
 
 echo "Añadiendo cambios..."
-git add .
+git add docs
 
-echo "Haciendo commit..."
-git commit -m "Actualización automática de GitHub Pages"
+# Verificar si hay cambios antes de hacer commit
+if git diff --cached --quiet; then
+    echo "No hay cambios para hacer commit."
+else
+    echo "Haciendo commit..."
+    git commit -m "Actualización automática de GitHub Pages"
+fi
 
 echo "Subiendo cambios a GitHub..."
 git push origin main
